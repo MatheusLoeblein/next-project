002dfa839aa33295b3cd591987ceba4e37bcf4aa
Matheus <matheus.eai@gmail.com>
2023-06-27 22:06:36 -0300

    First render in server styles principal feat next

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+const Heading = styled.h1`
+  ${({ theme }) => css`
+    background: ${theme.colors.secondaryColor};
+  `}
+`;
+
+export default function Home() {
+  return <Heading>Oi</Heading>
+}
